959e6111c348f99e441d53eee505f8217737e4cc
Ondrej Bajgar <ondrej@bajgar.org>
2026-09-22 23:14:56 +0200

    Week 1 practicals: Minimal corridor environment

--- week01_foundations/corridor_minimal.py
+++ week01_foundations/corridor_minimal.py
@@ -1,6 +1,5 @@
-"""The corridor in as few lines as possible. Compare with corridor.py."""
-
 import random
+from typing import Tuple
 
 import gymnasium as gym
 from gymnasium import spaces
@@ -8,18 +7,26 @@ from gymnasium import spaces
 
 class MinimalCorridor(gym.Env):
     def __init__(self, n_cells=8, slip=0.1):
+
         self.n_cells, self.slip = n_cells, slip
+
+        # Define gymnasium spaces:
         self.observation_space = spaces.Discrete(n_cells)
         self.action_space = spaces.Discrete(2)
 
-    def reset(self):
+    def reset(self) -> int:
         self.pos = 0
         return self.pos, {}
 
-    def step(self, action):
+    def step(self, action) -> Tuple[int, float, bool, bool, dict]:
+
         direction = 1 if action == 1 else -1
+
         if random.random() < self.slip:
             direction = -direction
+
         self.pos = min(max(self.pos + direction, 0), self.n_cells - 1)
         terminated = self.pos == self.n_cells - 1
+
         return self.pos, -1.0, terminated, False, {}
+
